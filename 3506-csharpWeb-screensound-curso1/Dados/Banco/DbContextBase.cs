acc9645a448247b8353dcfd68144b955d455e8fd
fabinho1992 <f.santosdev1992@gmail.com>
2024-04-23 09:06:24 -0300

    Camadas

---
+++ 3506-csharpWeb-screensound-curso1/Dados/Banco/DbContextBase.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Data.SqlClient;
+using ScreenSound.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ScreenSound.Banco;
+
+public class DbContextBase : DbContext// AQUI USO O PACOTE SQLCLIENT PARA FAZER A CONEXÃO COM O BANCO
+{
+    public DbSet<Artista> Artistas { get; set; }
+    public DbSet<Musica> Musicas { get; set; }
+
+    private string DbConnectionStringPc = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=DbScreenSoundV0;" +
+        "Integrated Security=True;Encrypt=False;" +
+        "Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.
+            UseSqlServer(DbConnectionStringPc)
+            .UseLazyLoadingProxies();
+    }
+
+
+    
+
+   
+
+    
+}
